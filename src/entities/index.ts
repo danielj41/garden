9c5c74798361d67560e3b7b783e0d551b9e3e9c5
Daniel Johnson <danielj41@gmail.com>
2019-02-11 22:23:38 -0800

    delete unused code

--- src/entities/index.ts
+++ src/entities/index.ts
@@ -1,28 +1,3 @@
-/*import MovingSquare, { MovingSquareState } from "./moving-square";
-
-import { combineReducers, Reducer } from "redux";
-import { Env, Task } from "../render/types";
-import { Entity, EntityAction, EntityState } from "./types";
-
-export const reducers = {
-  MovingSquare: MovingSquare.reducer
-};
-
-interface State {
-  [id: string]: EntityState;
-}
-
-export function reducer(state: State, action: EntityAction) {
-  const newState = {
-    ...state
-  };
-}
-
-export const renderers = {
-  MovingSquare: MovingSquare.render
-};
-*/
-
 import { Render } from "./types";
 import { Node } from "./node";
 
